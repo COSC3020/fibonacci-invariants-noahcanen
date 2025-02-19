0068130277059fc541a3d3c2aae71075818c3cd0
noahcanen <ncanen@uwyo.edu>
2025-02-19 12:42:59 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,23 @@
 function fib(n) {
-    return n;
+
+    var fibo = [0,1];
+    if (n == 0) {
+        fibo.pop();
+
+        return fibo;
+    }
+
+    var fibe = fibre(n,fibo);
+    return fibe;
+
+}
+function fibre(n, Fibonacci) {
+    if (n == Fibonacci.length-1) {
+        return Fibonacci;
+    }
+
+    Fibonacci[Fibonacci.length] = Fibonacci[Fibonacci.length-2]+Fibonacci[Fibonacci.length-1];
+
+    fibre(n, Fibonacci);
+    return Fibonacci;
 }
